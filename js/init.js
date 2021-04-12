a4037f554052f7433259e2a7ac9d5a3ddf8a3889
liz1808 <81264170+liz1808@users.noreply.github.com>
2021-04-12 14:35:56 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyDTORhF11V8eCGH07xcgfStMM3VzLa-xtk",
+  authDomain: "evaluacion3erparcial.firebaseapp.com",
+  databaseURL: "https://evaluacion3erparcial-default-rtdb.firebaseio.com",
+  projectId: "evaluacion3erparcial",
+  storageBucket: "evaluacion3erparcial.appspot.com",
+  messagingSenderId: "811857773266",
+  appId: "1:811857773266:web:d0cf872170472253ca230b",
+  measurementId: "G-5JQRLP139L"
+});
